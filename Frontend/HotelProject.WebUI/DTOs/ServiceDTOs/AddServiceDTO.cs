5d147ad11c2d5c73263a1f212c6d750fcbfcd536
UmutOncel <umutoncel91@gmail.com>
2024-03-22 18:20:46 +0300

    Frontend kısmında ServiceDTO ve Service controller oluşturuldu. CRUD işlemleri için action'lar ve view sayfaları yazıldı.

---
+++ Frontend/HotelProject.WebUI/DTOs/ServiceDTOs/AddServiceDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.DTOs.ServiceDTOs
+{
+    public class AddServiceDTO
+    {
+        [Required(ErrorMessage = "Hizmet ikon linki girmek zorunludur.")]
+        public string ServiceIcon { get; set; }
+
+        [Required(ErrorMessage = "Hizmet başlığı girmek zorunludur.")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı 100 karakterden fazla olamaz!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Hizmet açıklaması girmek zorunludur.")]
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/DTOs/ServiceDTOs/UpdateServiceDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.DTOs.ServiceDTOs
+{
+    public class UpdateServiceDTO
+    {
+        public int ServiceID { get; set; }
+
+        [Required(ErrorMessage = "Hizmet ikon linki girmek zorunludur.")]
+        public string ServiceIcon { get; set; }
+
+        [Required(ErrorMessage = "Hizmet başlığı girmek zorunludur.")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı 100 karakterden fazla olamaz!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Hizmet açıklaması girmek zorunludur.")]
+        [StringLength(500, ErrorMessage = "Hizmet açıklaması 500 karakterden fazla olamaz!")]
+        public string Description { get; set; }
+    }
+}
