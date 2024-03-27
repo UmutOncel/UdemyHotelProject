45423a84efb8a0a81e2252021ef81fda0d083a24
UmutOncel <umutoncel91@gmail.com>
2024-03-27 15:34:12 +0300

    Guest entity'si eklendi. Migration yapıldı. SQL kısmında Abouts tablosundaki RoomCount, StaffCount ve CustomerCount kısımlarının otomatik gelmesi için Trigger'lar yazıldı. Hotelier temada çeşitli düzenlemeler yapıldı.

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Guest
+    {
+        public int GuestID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+    }
+}
