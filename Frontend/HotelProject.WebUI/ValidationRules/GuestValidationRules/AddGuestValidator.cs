4cef3d7aaf587a398ee348c971b8dc2c1852ef54
UmutOncel <umutoncel91@gmail.com>
2024-03-28 15:25:05 +0300

    Fluent Validation için Frontend kısmında nuget'lar indirildi. Ayrı bir klasör açılıp AddGuest ve UpdateGuest için validation kuralları yazıldı. program.cs, Guest controller ve ilgli view sayfalarına gerekli kodlar eklendi.

---
+++ Frontend/HotelProject.WebUI/ValidationRules/GuestValidationRules/AddGuestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using HotelProject.WebUI.DTOs.GuestDTOs;
+
+namespace HotelProject.WebUI.ValidationRules.GuestValidationRules
+{
+    public class AddGuestValidator: AbstractValidator<AddGuestDTO> 
+    {
+        public AddGuestValidator()
+        {
+            RuleFor(x => x.Firstname).NotNull().WithMessage("İsim alanı boş geçilemez.")
+                                     .MinimumLength(2).WithMessage("İsim 2 karakterden az olamaz.")
+                                     .MaximumLength(30).WithMessage("İsim 30 karakterden fazla olamaz.");
+            
+            RuleFor(x => x.Lastname).NotNull().WithMessage("Soyisim alanı boş geçilemez.")
+                                    .MinimumLength(2).WithMessage("Soyisim 2 karakterden az olamaz.")
+                                    .MaximumLength(30).WithMessage("Soyisim 30 karakterden fazla olamaz.");
+
+            RuleFor(x => x.City).NotNull().WithMessage("Şehir alanı boş geçilemez.")
+                                .MinimumLength(3).WithMessage("Şehir 3 karakterden az olamaz.")
+                                .MaximumLength(20).WithMessage("Şehir 20 karakterden fazla olamaz.");
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/ValidationRules/GuestValidationRules/UpdateGuestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using HotelProject.WebUI.DTOs.GuestDTOs;
+
+namespace HotelProject.WebUI.ValidationRules.GuestValidationRules
+{
+    public class UpdateGuestValidator: AbstractValidator<UpdateGuestDTO>
+    {
+        public UpdateGuestValidator()
+        {
+            RuleFor(x => x.Firstname).NotNull().WithMessage("İsim alanı boş geçilemez.")
+                                     .MinimumLength(2).WithMessage("İsim 2 karakterden az olamaz.")
+                                     .MaximumLength(30).WithMessage("İsim 30 karakterden fazla olamaz.");
+
+            RuleFor(x => x.Lastname).NotNull().WithMessage("Soyisim alanı boş geçilemez.")
+                                    .MinimumLength(2).WithMessage("Soyisim 2 karakterden az olamaz.")
+                                    .MaximumLength(30).WithMessage("Soyisim 30 karakterden fazla olamaz.");
+
+            RuleFor(x => x.City).NotNull().WithMessage("Şehir alanı boş geçilemez.")
+                                .MinimumLength(3).WithMessage("Şehir 3 karakterden az olamaz.")
+                                .MaximumLength(20).WithMessage("Şehir 20 karakterden fazla olamaz.");
+        }
+    }
+}
