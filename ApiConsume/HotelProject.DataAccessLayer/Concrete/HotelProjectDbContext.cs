ded34f7b90eabd664bc538d89788e8be631a64e3
UmutOncel <umutoncel91@gmail.com>
2024-04-01 00:50:14 +0300

    SQL'de yazılan trigger'ların hata vermemesi için DbContext içine metot yazıldı. Default/Index içindeki yazım hataları giderildi.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/HotelProjectDbContext.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/HotelProjectDbContext.cs
@@ -16,6 +16,30 @@ namespace HotelProject.DataAccessLayer.Concrete
             optionsBuilder.UseSqlServer(@"Server=CASPER\SQL2022; Database=UdemyHotelProject; Trusted_Connection=True; TrustServerCertificate=True;");
         }
 
+        //SQL'de yazılan trigger'ların hata vermemesi için yazıldı.
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Room>(entry => 
+            {
+                entry.ToTable("Rooms", tb => tb.HasTrigger("RoomIncrease"));
+                entry.ToTable("Rooms", tb => tb.HasTrigger("RoomDecrease"));
+            });
+
+            builder.Entity<Staff>(entry => 
+            {
+                entry.ToTable("Staffs", tb => tb.HasTrigger("StaffIncrease"));
+                entry.ToTable("Staffs", tb => tb.HasTrigger("StaffDecrease"));
+            });
+
+            builder.Entity<Guest>(entry => 
+            {
+                entry.ToTable("Guests", tb => tb.HasTrigger("CustomerIncrease"));
+                entry.ToTable("Guests", tb => tb.HasTrigger("CustomerDecrease"));
+            });
+        }
+
         public DbSet<Room> Rooms{ get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Staff> Staffs { get; set; }
